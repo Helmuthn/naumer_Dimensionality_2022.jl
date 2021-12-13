0bab36273063950e53673cc91d5ae899e11fadf5
hnaumer2 <hnaumer2@illinois.edu>
2021-12-13 14:04:22 -0600

    Added phase plot generation

---
+++ PlotGeneration/PhasePlot/genie.jl
@@ -0,0 +1,212 @@
+include("helper.jl")
+
+##############################
+##### Initialize Figure ######
+##############################
+@info("Initializing Figure...")
+
+noto_sans = "./resources/NotoSans-Regular.ttf"
+
+tickfontsize    = 28
+labelfontsize   = 32
+
+f = Figure(font=noto_sans, resolution=(2000,400))
+
+
+####################################
+###### Plot Differential Form ######
+####################################
+@info("Plotting Differential Form...")
+
+vanderpol_arrow(u) = vanderpol(u,1,1)
+ax1 = Axis(f[1,1], aspect=1,
+            xticklabelsize=tickfontsize, 
+            yticklabelsize=tickfontsize, 
+            yticklabelpad=2,
+            xlabel="x", ylabel = "y",
+            xlabelsize=labelfontsize,
+            ylabelsize=labelfontsize)
+u_grid = -4:.333:4
+
+us = zeros(length(u_grid)^2)
+uv = zeros(length(u_grid)^2)
+i=1
+for y in u_grid
+    for x in u_grid
+        du = vanderpol_arrow([x,y])
+        du /= sqrt(sum(abs2,du))
+        us[i], uv[i] = du
+        global i += 1
+    end
+end
+
+us = reshape(us,length(u_grid),length(u_grid))/4
+uv = reshape(uv,length(u_grid),length(u_grid))/4
+
+
+arrows!(    ax1,
+            u_grid,
+            u_grid,
+            us,
+            uv)
+    
+xlims!(ax1, (-4,4))
+ylims!(ax1, (-4,4))
+
+
+###############################
+###### Plot Trajectories ######
+###############################
+
+ax2 = Axis( f[1,2], aspect=1,
+            xticklabelsize=tickfontsize, 
+            yticklabelsize=tickfontsize, 
+            yticklabelpad=2,
+            xlabel="x", ylabel = "y",
+            xlabelsize=labelfontsize,
+            ylabelsize=labelfontsize)
+
+# Four different initial conditions
+u0 = zeros(2,4)
+u0[2,:] = [-.1,.1,-4,4]
+
+for i in 1:4
+    μ = 1.0
+    tspan = (0.0,20.0)
+
+    prob = ODEProblem(vanderpol!,u0[:,i],tspan,μ)
+
+    sol = solve(prob,saveat=0.05)
+
+    lines!(ax2,sol, color=:black)
+end
+xlims!(ax2, (-4,4))
+ylims!(ax2, (-4,4))
+
+
+##########################################
+############ Phase Plot ##################
+##########################################
+@info("Generating Phase Plot...")
+
+# Determine period of limit cycle
+
+let
+  u0 = [1,1]
+  μ = 1.0
+  tspan = (0.0,20.0)
+  
+  prob = ODEProblem(vanderpol!,u0,tspan,μ)
+  
+  sol = solve(prob,saveat=0.01)
+  
+  
+  # Compute the distances from the final point and get one cycle
+  distance = zeros(2001-100)
+  for i in 1:2001-100
+      distance[i] = sum(abs2,sol[end] - sol[i])
+  end
+  ~,ind = findmin(distance)
+  period = 2001-ind
+  global positions = sol.u[end-period+1:end]
+  global positions = cat(positions...,dims=2)
+end
+
+
+u_grid = -4:.01:4
+
+results = zeros(length(u_grid)^2)
+
+Threads.@threads for i in 1:length(u_grid)
+  for j in 1:length(u_grid)
+    y = u_grid[j]
+    x = u_grid[i]
+    results[j + (i-1)*length(u_grid)] = phasemap([x,y],positions)
+  end
+end
+
+results = reshape(results,length(u_grid),length(u_grid))
+
+
+ax3 = Axis( f[1,3], 
+            aspect=1,
+            xticklabelsize=tickfontsize, 
+            yticklabelsize=tickfontsize, 
+            yticklabelpad=2,
+            xlabel="x", ylabel = "y",
+            xlabelsize=labelfontsize,
+            ylabelsize=labelfontsize)
+
+heatmap!(ax3, u_grid, u_grid, results, colormap=:phase)
+contour!(ax3, u_grid, u_grid, results, levels=31, color=:black)
+xlims!(ax3,(-4,4))
+ylims!(ax3,(-4,4))
+
+
+#####################################
+###### Generate Arrow Plot ##########
+#####################################
+@info("Plotting Informative Measurements...")
+
+u_grid = -4:.3333:4
+out_u = zeros(length(u_grid),length(u_grid))
+out_v = zeros(length(u_grid),length(u_grid))
+
+Threads.@threads for i in 1:length(u_grid)
+    for j in 1:length(u_grid)
+        u₀ = [u_grid[i], u_grid[j]]
+        dx = ForwardDiff.jacobian(simsystem,u₀)
+        ~, Σ, Vt = svd(dx)
+        # Force counterclockwise
+        test_vec = [u_grid[j], -u_grid[i]]
+        if dot(test_vec,Vt[1,:]) < 0
+            out_u[i,j] = Vt[1,1]
+            out_v[i,j] = Vt[1,2]
+        else
+            out_u[i,j] = -Vt[1,1]
+            out_v[i,j] = -Vt[1,2]
+        end
+    end
+end
+
+
+ax4 = Axis(f[1,4], aspect=1,
+            xticklabelsize=tickfontsize, 
+            yticklabelsize=tickfontsize, 
+            yticklabelpad=2,
+            xlabel="x", ylabel = "y",
+            xlabelsize=labelfontsize,
+            ylabelsize=labelfontsize)
+
+arrows!(    ax4,
+            u_grid,
+            u_grid,
+            out_u./4,
+            out_v./4)
+    
+xlims!(ax4, (-4,4))
+ylims!(ax4, (-4,4))
+
+##########################################
+########## Generate Streamplot ###########
+##########################################
+@info("Generating Informative Streamplot")
+
+ax5 = Axis(f[1,5], aspect=1,
+            xticklabelsize=tickfontsize, 
+            yticklabelsize=tickfontsize, 
+            yticklabelpad=2,
+            xlabel="x", ylabel = "y",
+            xlabelsize=labelfontsize,
+            ylabelsize=labelfontsize)
+
+streamplot!(ax5,(x,y) -> (Point2f0(informativeVec([x,y]))),-4..4,-4..4,maxsteps=10000, colormap=:seaborn_dark, density=.8)
+
+xlims!(ax5, (-4,4))
+ylims!(ax5, (-4,4))   
+
+##########################################
+############### Save Plot ################
+##########################################
+@info("Saving Figure...")
+save("out/demo.pdf",f);
---
+++ PlotGeneration/PhasePlot/helper.jl
@@ -0,0 +1,93 @@
+using CairoMakie
+using DiffEqSensitivity
+using Zygote
+using ForwardDiff
+using LinearAlgebra
+using DifferentialEquations
+
+"""
+  vanderpol!(du,u,μ,t)
+
+Van der Pol oscillator dynamics for DifferentialEquations.jl
+inline
+"""
+function vanderpol!(du,u,μ,t)
+    x,y = u
+    du[1] = μ * (x - x^3/3 - y)
+    du[2] = x/μ
+end
+
+"""
+  vanderpol(u,μ,t)
+
+Van der Pol oscillator dynamics for DifferentialEquations.jl 
+not inline
+"""
+function vanderpol(u,μ,t)
+    du = zeros(2)
+    vanderpol!(du,u,μ,t)
+    return du
+end
+
+
+"""
+  phasemap(u, positions)
+
+Applies to Van der Pol oscillator.
+
+Assigns a point `u` in the statespace to a position on the 
+limit cycle of the oscillator. Given a list of `positions`
+spaced equally in time along the limit cycle, this function
+simulates the system from initial condition `u` then determines
+the closest element in `positions`
+
+Returns: The index of the closest element.
+"""
+function phasemap(u, positions)
+    μ = 1.0
+    tspan = (0.0,40.0)
+
+    prob = ODEProblem(vanderpol!,u,tspan,μ)
+
+    sol = solve(prob)
+    final = sol.u[end]
+    ~, out = findmin(sum(abs2,positions .- final,dims=1))
+    return out[2]
+end
+
+
+"""
+  simystem(u)
+
+Compute the value of the Van der Pol oscillator initialized at
+`u` 20 units of time in the future.
+"""
+function simsystem(u)
+    μ = 1.0
+    tspan = (0.0,20.0)
+
+    prob = ODEProblem(vanderpol!,u,tspan,μ)
+
+    sol = solve(prob,rtol=1e-6,atol=1e-6,sensealg=QuadratureAdjoint())
+    return sol.u[end]
+end
+
+"""
+  informativeVec(u)
+
+Find the non-zero eigenvalue of dφ₂₀ in the Van Der Pol
+oscillator around state space point `u`. 
+"""
+function informativeVec(u)
+    dx = ForwardDiff.jacobian(simsystem,u)
+    ~, Σ, Vt = svd(dx)
+    # Force counterclockwise
+    test_vec = [u[2], -u[1]]
+    if dot(test_vec,Vt[1,:]) > 0
+        return Vt[1,:]
+    else
+        return -Vt[1,:]
+    end
+end
+
+
