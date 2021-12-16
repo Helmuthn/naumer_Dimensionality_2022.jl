accc5092de8cd35bf66244c20cc5aa81aff28ef2
Helmuth Naumer <hnaumer2@illinois.edu>
2021-12-16 13:28:06 -0600

    Added AbstractSystem and LinearSystem

--- src/systems.jl
+++ src/systems.jl
@@ -1,1 +1,30 @@
-# Dynamical system defintions used in the manuscript
+# Dynamical system definitions used in the manuscript
+
+############################
+####### Basic System #######
+############################
+
+abstract type AbstractSystem{T} end;
+
+#############################
+###### Linear Systems #######
+#############################
+
+# Define a struct containing the relevant details
+struct LinearSystem{T} <: AbstractSystem{T}
+    dynamics::Matrix{T}
+end
+
+function differential(system::LinearSystem, x::Vector{T})
+    return system.dynamics * x
+end
+
+function flow(x::Vector{T}, τ, system::LinearSystem)
+    return exp(τ * system.dynamics) * x
+end
+
+function jacobian(x::Vector{T}, τ, system::Linearsystem)
+    return exp(τ * system.dynamics)
+end
+
+
