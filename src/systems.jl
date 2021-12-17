fd019793032b588b9a85319d36b94c9bcbc9dbfc
Helmuth Naumer <hnaumer2@illinois.edu>
2021-12-16 22:02:26 -0600

    Added documented generic functions

--- src/systems.jl
+++ src/systems.jl
@@ -6,6 +6,54 @@
 
 abstract type AbstractSystem{T} end;
 
+"""
+    differential(system::AbstractSystem, x::Vector)
+
+Computes the time derivative of a given continous-time dynamical
+system evaluated at a given point.
+
+### Arguments
+ - `system` - Continuous-Time Dynamical System
+ - `x`      - Point to Evaluate differential
+
+### Returns
+The derivative of the trajectory of the system with respect to time
+evaluated at a fixed point `x`.
+"""
+function differential end;
+
+"""
+    flow(system::AbstractSystem, τ, x::Vector)
+
+Advances the state of a dynamical `system` from state `x` by time `τ`.
+
+### Arguments
+ - `system` - Continous-Time Dynamical System
+ - `τ`      - Advancement time
+ - `x`      - Initial state
+
+### Returns
+The state advanced by `τ` unties of time
+"""
+function flow end;
+
+"""
+    flowJacobian(system::AbstractSystem, τ, x::Vector)
+
+Computes the Jacobian of the flow of a `system` for `τ` units of time 
+around an initial condition `x`.
+
+### Arguments
+ - `system` - Continous-Time Dynamical System
+ - `τ`      - Advancement time
+ - `x`      - Initial State
+
+### Returns
+Jacobian matrix of the flow representing the derivative with respect to the
+initial state `x`.
+"""
+function flowJacobian end;
+
 export differential, flow, flowJacobian
 
 #############################
